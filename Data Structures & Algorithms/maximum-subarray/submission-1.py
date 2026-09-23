e39a55d35ff60e1fb0b372a47c6cf13c175e1031
silversurfer <134323590+andrewtclim@users.noreply.github.com>
2026-09-23 13:30:11 -0700

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Kadanes Algorithim
+        # Record the sum if its positive, otherwise reset the sum to zero
+
+        maxSum = nums[0]
+        curSum = 0
+
+        for num in nums:
+            # reset negative sums to 0
+            if curSum < 0: 
+                curSum = 0 
+            curSum += num 
+            # record and update the maxSum 
+            maxSum = max(maxSum, curSum)
+        
+        return maxSum 
